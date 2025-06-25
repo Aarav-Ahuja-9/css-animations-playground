df3a461abf4b77a5c7711cade8c334643bd11f17
mr.aaravahuja <mr.aaravahuja@gmail.com>
2025-06-25 08:00:33 +0000

    not working using js

--- main.js
+++ main.js
@@ -14,23 +14,11 @@ button.addEventListener("click", (e) => {
   let vlaueOfSelectAnimation = selectAnimationField.value;
   previewBox.setAttribute("class", "animate__animated");
   previewBox.classList.add(vlaueOfSelectAnimation);
-  previewBox.style.animationIterationCount = animationCountValue;
   console.log(previewBox.style);
-  if (animationCountValue == "infinite") {
     setTimeout(() => {
       previewBox.classList.remove(vlaueOfSelectAnimation);
       previewBox.classList.remove("animate__animated");
-      previewBox.style.animationIterationCount = "";
-      console.log(previewBox.style.animationIterationCount);
-    }, 50000);
-  } else if (animationCountValue > 0) {
-    setTimeout(() => {
-      previewBox.classList.remove(vlaueOfSelectAnimation);
-      previewBox.classList.remove("animate__animated");
-      previewBox.style.animationIterationCount = "";
-      console.log(previewBox.style.animationIterationCount);
     }, animationCountValue * 1000);
-  }
 
   // console.log(vlaueOfSelectAnimation);
   // console.log(previewBox.classList);
